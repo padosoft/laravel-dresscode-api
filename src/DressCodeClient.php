29ef4d59393f4d7a53f243047b10581cfb05b98a
Marco Bramato <bramatom@gmail.com>
2023-03-01 17:03:49 +0100

    Fix

--- src/DressCodeClient.php
+++ src/DressCodeClient.php
@@ -14,7 +14,7 @@ use DressCodeKeyTrait;
         $hub_key = config('dresscode-api-settings.hub_key');
         $subscription_key = config('dresscode-api-settings.subscription_key');
         $this->initKey($username, $password, $hub_key, $subscription_key);
-        $response = DressCodeClientApi::create($this->dressCodeKey)->postUpload($this->dressCodeKey, $products);
+        $response = DressCodeClientApi::create($this->dressCodeKey)->postUpload($this->dressCodeKey->hub_key, $products);
         return $response;
     }
 
